b7f2a9b60f92a3f0ab9120e8f80d5f95aba07bac
kameshwar89 <54500833+kameshwar89@users.noreply.github.com>
2019-10-12 14:55:21 +0530

    Update Face Prediction.py

--- Face Prediction.py
+++ Face Prediction.py
@@ -1,115 +1,109 @@
-import pickle
+from face_reco_image import FaceImage
+import pandas as pd
+import random
+import time
 import os
-import cv2
-import numpy as np
-import tensorflow as tf
-from scipy import misc
-import align.detect_face
-import facenet
+import shutil
+import tempfile
+import logging
+from flask import jsonify
+from flask import json
+from flask import request
+from flask import Flask , render_template
+import pandas as pd
+pd.set_option("display.colheader_justify","left")
+import dlib
+from os import path, getcwd
+import tensorflow as tf 
 
-gpu_memory_fraction = 0.3
-facenet_model_checkpoint = os.path.dirname(__file__) + "/../model_checkpoints/20170512-110547"
-classifier_model = os.path.dirname(__file__) + "/../model_checkpoints/my_classifier_1.pkl"
-debug = False
+print ("dlib version: {}".format(dlib.__version__))
 
-class Face:
-    def __init__(self):
-        self.name = None
-        self.bounding_box = None
-        self.image = None
-        self.container_image = None
-        self.embedding = None
+USE_SMALL_FRAME = False
+VISUALIZE_DATASET = False
+process_this_frame = True
+face = FaceImage()
+graph = tf.get_default_graph()
+app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+app.static_folder = 'static'
+app.secret_key = 'super secret key'
 
-class Recognition:
-    def __init__(self):
-        self.detect = Detection()
-        self.encoder = Encoder()
-        self.identifier = Identifier()
-
-    def add_identity(self, image, person_name):
-        faces = self.detect.find_faces(image)
-
-        if len(faces) == 1:
-            face = faces[0]
-            face.name = person_name
-            face.embedding = self.encoder.generate_embedding(face)
-            return faces
-
-    def identify(self, image):
-        faces = self.detect.find_faces(image)
-
-        for i, face in enumerate(faces):
-            if debug:
-                cv2.imshow("Face: " + str(i), face.image)
-            face.embedding = self.encoder.generate_embedding(face)
-            face.name = self.identifier.identify(face)
-
-        return faces
+temporary_directory = tempfile.mkdtemp()
+_allow_origin = '*'
+_allow_methods = 'PUT, GET, POST, DELETE, OPTIONS'
+_allow_headers = 'Authorization, Origin, Accept, Content-Type, X-Requested-With'
 
 
-class Identifier:
-    def __init__(self):
-        with open(classifier_model, 'rb') as infile:
-            self.model, self.class_names = pickle.load(infile)
-
-    def identify(self, face):
-        if face.embedding is not None:
-            predictions = self.model.predict_proba([face.embedding])
-            best_class_indices = np.argmax(predictions, axis=1)
-            return self.class_names[best_class_indices[0]]
+@app.errorhandler(400)
+def bad_request(e):
+    return jsonify({"status": "not ok", "message": "this server could not understand your request"})
 
 
-class Encoder:
-    def __init__(self):
-        self.sess = tf.Session()
-        with self.sess.as_default():
-            facenet.load_model(facenet_model_checkpoint)
-
-    def generate_embedding(self, face):
-        images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
-        embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
-        phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
-
-        prewhiten_face = facenet.prewhiten(face.image)
-
-        feed_dict = {images_placeholder: [prewhiten_face], phase_train_placeholder: False}
-        return self.sess.run(embeddings, feed_dict=feed_dict)[0]
+@app.errorhandler(404)
+def not_found(e):
+    return jsonify({"status": "not found", "message": "route not found"})
 
 
-class Detection:
-    minsize = 20  
-    threshold = [0.6, 0.7, 0.7]  
-    factor = 0.709
+@app.errorhandler(500)
+def notfound(e):
+    return jsonify({"status": "internal error", "message": "internal error occurred in server"})
 
-    def __init__(self, face_crop_size=160, face_crop_margin=32):
-        self.pnet, self.rnet, self.onet = self._setup_mtcnn()
-        self.face_crop_size = face_crop_size
-        self.face_crop_margin = face_crop_margin
 
-    def _setup_mtcnn(self):
-        with tf.Graph().as_default():
-            gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_memory_fraction)
-            sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
-            with sess.as_default():
-                return align.detect_face.create_mtcnn(sess, None)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    def find_faces(self, image):
-        faces = []
 
-        bounding_boxes, _ = align.detect_face.detect_face(image, self.minsize,self.pnet, self.rnet, self.onet,self.threshold, self.factor)
-        for bb in bounding_boxes:
-            face = Face()
-            face.container_image = image
-            face.bounding_box = np.zeros(4, dtype=np.int32)
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
-            img_size = np.asarray(image.shape)[0:2]
-            face.bounding_box[0] = np.maximum(bb[0] - self.face_crop_margin / 2, 0)
-            face.bounding_box[1] = np.maximum(bb[1] - self.face_crop_margin / 2, 0)
-            face.bounding_box[2] = np.minimum(bb[2] + self.face_crop_margin / 2, img_size[1])
-            face.bounding_box[3] = np.minimum(bb[3] + self.face_crop_margin / 2, img_size[0])
-            cropped = image[face.bounding_box[1]:face.bounding_box[3], face.bounding_box[0]:face.bounding_box[2], :]
-            face.image = misc.imresize(cropped, (self.face_crop_size, self.face_crop_size), interp='bilinear')
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+     return render_template('upload.html')
 
-            faces.append(face)
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    global graph
+    
+    with graph.as_default():
+        file = request.files.get('upload')
+        filename, ext = os.path.splitext(file.filename)
+        if ext not in ('.png', '.jpg', '.jpeg'):
+            return 'File extension not allowed.'
+        tmp = tempfile.TemporaryDirectory()
+        temp_storage = path.join(tmp.name, file.filename)
 
-        return faces
+        file.save(temp_storage)
+
+        result_info = face.detect_face_info(temp_storage)
+        output_fm = pd.DataFrame(result_info)
+        output_fm = output_fm[['Name','Age','Gender','Imotion']]
+        output_fm.columns = ['Name','Age','Gender','Emotion']
+        output_fm['Ethnicity'] = "" 
+        #output_fm.set_index('Name',inplace=True)
+        Ethnicty  = ['American Indian or Alaska Native','Asian','Black or African American','Hispanic or Latino','Native Hawaiian or Other Pacific Islander','White']
+        for i in range(len(output_fm)):
+            rand_ethn = random.choice(Ethnicty)
+            output_fm['Ethnicity'][i] = rand_ethn  
+        output_fm.style.set_properties(align="left")
+    return render_template('classify.html',data=output_fm.to_html())    
+
+
+# @app.route("/charts")
+# def charts(): 
+#              # ** Printing out the Accuracy,      
+#              Accuracy=metrics.accuracy_score(y_test, y_predictions)*100
+#              return render_template('charts.html', data=str(Accuracy))
+
+
+
+if __name__ == "__main__":
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.ERROR)
+
+    print ("Starting server on http://localhost:5000")
+    print ("Serving ...",  app.run(host='0.0.0.0'))
+    print ("Finished !")
+    print ("Removing temporary directory ...",shutil.rmtree(temporary_directory))
+    print ("Done !")
